9483112e6e8c4ef7666504a22952bd76ddc53f02
Tanat Samuttha <163229612+TanatSamuttha@users.noreply.github.com>
2025-11-22 03:04:23 +0700

    🖥️ Creat AddDebt function&route

--- main.go
+++ main.go
@@ -4,12 +4,14 @@ import "github.com/gofiber/fiber/v2"
 
 type Debt struct {
 	ID           int    `json: "id"`
-	creditorID   int    `json: creditor_id`
+	creditorID   int    `json: "creditor_id"`
 	creditorName string `json: "creditor_name"`
 	debtorID     int    `json: debtor_id`
 	debtorName   string `json: debtor_name`
 	amount       int    `json: amount`
 }
+
+var dummyDataBase []Debt
 
 func AddDebt(c *fiber.Ctx) error {
 	newDebt := new(Debt)
@@ -17,11 +19,16 @@ func AddDebt(c *fiber.Ctx) error {
 	if err := c.BodyParser(newDebt); err != nil {
 		return c.SendStatus(fiber.StatusBadRequest)
 	}
+
+	dummyDataBase = append(dummyDataBase, *newDebt)
+
 	return c.JSON(newDebt)
 }
 
 func main() {
 	app := fiber.New()
+
+	app.Get("/AddDebt", AddDebt)
 
 	app.Listen(":8080")
 }

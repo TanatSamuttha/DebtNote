71f6e95bfbae088c85a2841ffc3bf293969894bb
Tanat Samuttha <163229612+TanatSamuttha@users.noreply.github.com>
2025-11-22 02:28:04 +0700

    🖥️ Create debt struct

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+type Debt struct{
+	ID int `json: "id"`
+	creditorID int `json: creditor_id`
+	creditorName string `json: "creditor_name"`
+	debtorID int `json: debtor_id`
+	debtorName string `json: debtor_name`
+	amount int `json: amount`
+}
+
+func main(){
+
+}

16d8540f12ed8933255de2bda68590b737429e5f
Tanat Samuttha <163229612+TanatSamuttha@users.noreply.github.com>
2025-11-22 02:31:47 +0700

    🖥️ Create app

--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
-type Debt struct{
-	ID int `json: "id"`
-	creditorID int `json: creditor_id`
+import "github.com/gofiber/fiber"
+
+type Debt struct {
+	ID           int    `json: "id"`
+	creditorID   int    `json: creditor_id`
 	creditorName string `json: "creditor_name"`
-	debtorID int `json: debtor_id`
-	debtorName string `json: debtor_name`
-	amount int `json: amount`
+	debtorID     int    `json: debtor_id`
+	debtorName   string `json: debtor_name`
+	amount       int    `json: amount`
 }
 
-func main(){
+func main() {
+	app := fiber.New()
 
+	app.Listen(":8080")
 }

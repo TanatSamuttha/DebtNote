20dfad94c60bc6c00f7322f66460c878c279a24f
Tanat Samuttha <163229612+TanatSamuttha@users.noreply.github.com>
2025-11-28 00:46:27 +0700

    🖥️  CreateDatabase connection

--- main.go
+++ main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"strconv"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 type Debt struct {
-	ID           int    `json:"id"`
+	gorm.Model
 	CreditorID   int    `json:"creditorid"`
-	CreditorName string `json:"creditorname"`
+	Creditor     User	`gorm:"foreignKey:CreditorID"`
 	DebtorID     int    `json:"debtorid"`
-	DebtorName   string `json:"debtorname"`
+	Debtor		 User	`gorm:"foreignKey:DebtorID"`
 	Amount       int    `json:"amount"`
 }
 
@@ -17,6 +24,12 @@ type ShowDebt struct {
 	CreditorName string `json:"creditorname"`
 	DebtorName   string `json:"debtorname"`
 	Amount       int    `json:"amount"`
+}
+
+type User struct {
+	gorm.Model
+	UserName string `json:"username"`
+	Password string `json:"password"`
 }
 
 var dummyDataBase []Debt
@@ -33,24 +46,69 @@ func AddDebt(c *fiber.Ctx) error {
 	return c.JSON(newDebt)
 }
 
-func GetDebts(c *fiber.Ctx) error {
-	var debts []ShowDebt
-	for _, debt := range dummyDataBase {
-		debts = append(debts, ShowDebt{
-			CreditorName: debt.CreditorName,
-			DebtorName: debt.DebtorName,
-			Amount: debt.Amount,
-		})
+// func GetDebts(c *fiber.Ctx) error {
+// 	var debts []ShowDebt
+// 	for _, debt := range dummyDataBase {
+// 		debts = append(debts, ShowDebt{
+// 			CreditorName: debt.CreditorName,
+// 			DebtorName: debt.DebtorName,
+// 			Amount: debt.Amount,
+// 		})
+// 	}
+// 	return c.JSON(debts)
+// }
+
+func getStringEnv(key string, fallback string) string {
+	if value, exist := os.LookupEnv(key); exist {
+		return value
 	}
-	return c.JSON(debts)
+	return fallback
 }
 
+func getIntEnv(key string, fallback int) int {
+	if value, exist := os.LookupEnv(key); exist {
+		if returnValue, err := strconv.Atoi(value); err == nil {
+			return returnValue
+		}
+	}
+	return fallback
+}
+
+// var (
+// 	host = getStringEnv("DB_Host", "localhost")
+// 	port = getIntEnv("DB_Port", 5432)
+// 	user = getStringEnv("DB_UserName", "postgres")
+// 	password = getStringEnv("DB_Password", "sharkyXD")
+// 	dbname = "mydatabase"
+// )
+
 func main() {
-	app := fiber.New()
+	err := godotenv.Load()
+	if err != nil {
+		panic("Load ENV fail")
+	}
+	host := getStringEnv("DB_Host", "localhost")
+	port := getIntEnv("DB_Port", 5432)
+	user := getStringEnv("DB_UserName", "")
+	password := getStringEnv("DB_Password", "")
+	dbname := "mydatabase"
+	fmt.Println("port", port)
+	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("Database connect failed")
+	}
 
-	app.Post("/AddDebt", AddDebt)
+	err = db.AutoMigrate(&User{}, &Debt{})
+	if err != nil {
+		panic(err)
+	}
 
-	app.Get("/GetDebts", GetDebts)
+	// app := fiber.New()
 
-	app.Listen(":8080")
+	// app.Post("/AddDebt", AddDebt)
+
+	// app.Get("/GetDebts", GetDebts)
+
+	// app.Listen(":8080")
 }

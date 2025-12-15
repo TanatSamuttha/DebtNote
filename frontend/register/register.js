ea18201a36e36f3f57f9f1b352a92a86880f100e
Tanat Samuttha <163229612+TanatSamuttha@users.noreply.github.com>
2025-12-15 23:18:07 +0700

    🖥️Create register page

---
+++ frontend/register/register.js
@@ -0,0 +1,18 @@
+document.getElementById("register-form").addEventListener("submit", register)
+
+function register(event){
+    event.preventDefault()
+    const username = document.forms["register-form"]["username"].value;
+    const password = document.forms["register-form"]["password"].value;
+    axios({
+        method: 'post',
+        url: 'http://127.0.0.1:8080/register',
+        data: {
+            username: username,
+            password: password
+        }
+    })
+    .then(res => {
+        console.log(res.data)
+    })
+}
